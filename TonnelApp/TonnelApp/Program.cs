12d72dde0c5caaab3ced695123b8ae5727f9f696
vadimmm972 <vadimmm972@gmail.com>
2020-09-26 18:18:23 +0300

    no message

--- TonnelApp/TonnelApp/Program.cs
+++ TonnelApp/TonnelApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,8 @@ namespace TonnelApp
     {
         static void Main(string[] args)
         {
+            Services.Simulator simulatore = new Services.Simulator();
+            simulatore.GenerateTunnelFromFile(6, Directory.GetCurrentDirectory()+"\\Generation\\Initialization.txt");
         }
     }
 }
---
+++ TonnelApp/TonnelApp/Services/Simulator.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TonnelApp.Interfaces;
+using TonnelApp.Models;
+
+namespace TonnelApp.Services
+{
+    class Simulator : ITunnelGenerator
+    {
+        public delegate void SimulatorHandler(string message);
+        public event SimulatorHandler Notify;
+        private List<SegmentModel> Segments;
+        private Random rand;
+        public Simulator()
+        {
+            Segments = new List<SegmentModel>();
+            rand = new Random();
+        }
+        public void GenerateRandomTunnel(int tunnelLength)
+        {
+            try
+            {
+                for (int i = 0; i < tunnelLength; i++)
+                {
+                    Segments.Add(new SegmentModel() { IsLightOn = rand.Next(0, 2) });
+                }
+            }
+            catch (Exception ex)
+            {
+                Notify?.Invoke($"Simulator (GenerateRandomTunnel)\n Error: {ex.Message}\n");
+            }
+        }
+        public void GenerateTunnelFromFile(int tunnelLength, string inputFileName)
+        {
+            try
+            {
+                int sizeFile = 0;
+                using (StreamReader sr = new StreamReader(inputFileName, Encoding.Default))
+                {
+                    string line;
+
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        if (!String.IsNullOrEmpty(line))
+                        {
+                            Segments.Add(new SegmentModel() { IsLightOn = Convert.ToInt32(line) });
+                            sizeFile++;
+                        }
+                    }
+                }
+                if (tunnelLength > sizeFile)
+                {
+                    int number;
+                    using (StreamWriter sw = new StreamWriter(inputFileName, true, Encoding.Default))
+                    {
+                        for (int j = 0; j < tunnelLength - sizeFile; j++)
+                        {
+                            number = rand.Next(0, 2);
+                            sw.WriteLine(number);
+                            Segments.Add(new SegmentModel() { IsLightOn = number });
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Notify?.Invoke($"Simulator (GenerateTunnelFromFile)\n Error: {ex.Message}\n");
+            }
+
+        }
+    }
+}
