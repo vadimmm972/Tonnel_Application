72037cb6b60079079ac4133de2b4c06448611ec2
vadimmm972 <vadimmm972@gmail.com>
2020-09-26 15:42:07 +0300

    The base architecture has been added to project

---
+++ TonnelApp/TonnelApp/Interfaces/ISimulator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TonnelApp.Interfaces
+{
+    interface ISimulator
+    {
+        // Method returns true if the light in the current segment is on
+        bool IsLightOn();
+
+        // Method moves the robot forward
+        void MoveForward();
+
+        // Method moves the robot to the segment back
+        void MoveBackward();
+
+        // Method changes state of the light bulb (from on to off and vice versa)
+        void SwitchLight();
+
+    }
+}
